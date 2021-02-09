9199dad624c156005c15d050fa13e819cd7d550b
jirayu123za <76035470+jirayu123za@users.noreply.github.com>
2021-02-09 17:15:11 +0700

    finish1

--- lab16_2.cpp
+++ lab16_2.cpp
@@ -1,19 +1,5 @@
-#include<iostream>
-using namespace std;
-
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
-}
-
-int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+void myString(char *&p,int M){
+    p = new char[M+1];
+    for(int i = 0; i < M;i++) p[i] = 'A'+i;
+    p[M]= 0;
 }
